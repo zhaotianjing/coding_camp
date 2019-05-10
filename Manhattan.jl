aaf8a6b3ef4fb1bf8b097f488f432391bc65ee97
zhaotianjing <tjzhao@ucdavis.edu>
2019-05-10 13:02:47 -0700

    Update Manhattan.jl

--- Manhattan.jl
+++ Manhattan.jl
@@ -5,7 +5,7 @@ using DataFrames
 data = CSV.read("C:/Users/ztjsw/Desktop/data.txt", delim = ',', header = true)
 
 data[:chromosome] = string.(data[:chromosome])
-data[:neg_log_p] = -log10.(dat[:pvalue])
+data[:neg_log_p] = -log10.(data[:pvalue])
 
 
 #calculate adjusted position
@@ -20,15 +20,17 @@ data_new[:adj_pos] = data_new[:position] + data_new[:cumsum]
 x_chrom_pos = by(data_new, :chromosome, mid_pos = :adj_pos => d -> (maximum(d) + minimum(d))/2)[2]
 x_chrom_label = unique(data_new[:chromosome])
 
-function pos_to_chrom(x_chrom_pos, x_chrom_label, iPos)
+function pos_to_chrom(iPos)
     # return the name of its chromosome for a position
     return x_chrom_label[x_chrom_pos .== iPos][1]
 end
 
 
-plot(data_new, x=:adj_pos, y=:neg_log_p, color=:chromosome, Geom.point,
+p=plot(data_new, x=:adj_pos, y=:neg_log_p, color=:chromosome, Geom.point,
         Guide.xlabel("Chromosome"),
         Guide.ylabel("-log<sub>10</sub>(p)"),
         Guide.xticks(ticks=x_chrom_pos),
         Scale.x_continuous(labels = pos_to_chrom),  #labels is a function which maps a coordinate value to a string label
         Theme(key_position = :none, grid_line_width=0mm, point_size=0.1cm))
+
+display(p)
